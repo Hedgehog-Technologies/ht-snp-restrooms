7bb1aa88c822b615db5cf44d42ce26c1070d7b42
Jag <jag3dagster@gmail.com>
2024-08-01 02:40:59 -0700

    Initial content check-in

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+this_is_a_map 'yes'
+
+files {
+    'stream/ytyp/ht_joshbog.ytyp',
+    'audio/97049435_game.dat151.rel'
+}
+
+data_file 'DLC_ITYP_REQUEST' 'stream/ytyp/ht_joshbog.ytyp'
+data_file 'AUDIO_GAMEDATA' 'audio/97049435_game.dat'
